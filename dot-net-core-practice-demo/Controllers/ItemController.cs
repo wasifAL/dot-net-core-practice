c5190513822b9ed2be8e5831e162924b5d29d5ac
arminhaque <armin.haque06@gmail.com>
2021-11-11 15:55:35 +0600

    11.11.2021

---
+++ dot-net-core-practice-demo/Controllers/ItemController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using dot_net_core_practice_demo.Repositories;
+using dot_net_core_practice_demo.Entities;
+
+namespace dot_net_core_practice_demo.Controllers
+{
+    [ApiController]
+    [Route("item")]
+    public class ItemController : ControllerBase
+    {
+        private readonly InMemItemRepositories repository;
+
+        public ItemController()
+        {
+            repository = new InMemItemRepositories();
+        }
+                       
+        [HttpGet]
+        public IEnumerable<Item> GetItems()
+        {
+            var items = repository.GetItems();
+            return items;
+        }
+    }
+   
+
+    
+}
---
+++ dot-net-core-practice-demo/Entities/Item.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dot_net_core_practice_demo.Entities
+{
+    public record Item
+    {
+      public Guid Id { get; init; }
+      public string Name { get; init; }
+      public decimal Price { get; init; }
+      public DateTimeOffset CreatedDate { get; init; }
+            
+            
+            
+    }
+}
